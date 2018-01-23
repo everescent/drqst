03f6cb5ae23011ec505cba6f743f614ee25b93e8
everescent <35443878+everescent@users.noreply.github.com>
2018-01-23 23:55:54 +0800

    Added new filter

--- Production/DragonAttack/DragonAttack/main.cpp
+++ Production/DragonAttack/DragonAttack/main.cpp
@@ -1,11 +1,16 @@
 #include "AEEngine.h"
 
+#define R_WIDTH 1280
+#define R_HEIGHT 720
+
+
 int WINAPI WinMain(HINSTANCE instanceH, HINSTANCE prevInstanceH, LPSTR command_line, int show)
 {
-	UNREFERENCED_PARAMETER(instanceH);
-	UNREFERENCED_PARAMETER(prevInstanceH);
-	UNREFERENCED_PARAMETER(command_line);
-	UNREFERENCED_PARAMETER(show);
-
-	AESysInit(instanceH, show, 800, 600, 1, 60, NULL);
+  UNREFERENCED_PARAMETER(instanceH);
+  UNREFERENCED_PARAMETER(prevInstanceH);
+  UNREFERENCED_PARAMETER(command_line);
+  UNREFERENCED_PARAMETER(show);
+  
+  AESysInit(instanceH, show, R_WIDTH, R_HEIGHT, true, 60, NULL);
+  while(AEVK_0)
 }
