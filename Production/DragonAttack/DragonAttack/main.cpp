05c9b49f1e6f3f72bffe59021fb31d45b291eb31
everescent <35443878+everescent@users.noreply.github.com>
2018-01-24 00:29:58 +0800

    Still trying to ignore useless files

--- Production/DragonAttack/DragonAttack/main.cpp
+++ Production/DragonAttack/DragonAttack/main.cpp
@@ -4,18 +4,14 @@
 #define R_WIDTH 1280
 #define R_HEIGHT 720
 
-
 int WINAPI WinMain(HINSTANCE instanceH, HINSTANCE prevInstanceH, LPSTR command_line, int show)
 {
   UNREFERENCED_PARAMETER(instanceH);
   UNREFERENCED_PARAMETER(prevInstanceH);
   UNREFERENCED_PARAMETER(command_line);
   UNREFERENCED_PARAMETER(show);
-  
-  AESysInit(instanceH, show, R_WIDTH, R_HEIGHT, true, 60, NULL);
-  std::cin.get();
-  std::cin.get();
 
-  AESysExit();
+  AESysInit(instanceH, show, R_WIDTH, R_HEIGHT, true, 60, NULL);
+  system("pause");
   return 0;
 }
