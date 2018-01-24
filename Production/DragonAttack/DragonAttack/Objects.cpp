63c17e7166bc24f9cd64fdac613c8fe2b55c8b03
everescent <35443878+everescent@users.noreply.github.com>
2018-01-24 19:41:12 +0800

    Added Object Class and Functions to render object

--- Production/DragonAttack/DragonAttack/Objects.cpp
+++ Production/DragonAttack/DragonAttack/Objects.cpp
@@ -31,3 +31,56 @@ AEGfxVertexList* CreateRectangle(float width, float height, unsigned int color)
   return AEGfxMeshEnd();
 }
 
+AEGfxVertexList* CreateFloor(float Multiplier, unsigned int color)
+{
+	AEGfxMeshStart();
+	AEGfxTriAdd(
+		-FLOOR_WIDTH * Multiplier, -FLOOR_HEIGHT * Multiplier, color, 0.0f, 0.0f,
+		FLOOR_WIDTH * Multiplier, -FLOOR_HEIGHT * Multiplier, color, 0.0f, 0.0f,
+		-FLOOR_WIDTH * Multiplier, FLOOR_HEIGHT * Multiplier, color, 0.0f, 0.0f);
+	AEGfxTriAdd(
+		FLOOR_WIDTH * Multiplier, -FLOOR_HEIGHT * Multiplier, color, 0.0f, 0.0f,
+		FLOOR_WIDTH * Multiplier, FLOOR_HEIGHT * Multiplier, color, 0.0f, 0.0f,
+		-FLOOR_WIDTH * Multiplier, FLOOR_HEIGHT * Multiplier, color, 0.0f, 0.0f);
+
+	return AEGfxMeshEnd();
+}
+
+AEGfxVertexList* CreatePlatform(float MultiplierW, float MultiplierH, unsigned int color)
+{
+	AEGfxMeshStart();
+	AEGfxTriAdd(
+		-PLAT_WIDTH * MultiplierW, -PLAT_HEIGHT * MultiplierH, color, 0.0f, 0.0f,
+		PLAT_WIDTH * MultiplierW, -PLAT_HEIGHT * MultiplierH, color, 0.0f, 0.0f,
+		-PLAT_WIDTH * MultiplierW, PLAT_HEIGHT * MultiplierH, color, 0.0f, 0.0f);
+	AEGfxTriAdd(
+		PLAT_WIDTH * MultiplierW, -PLAT_HEIGHT * MultiplierH, color, 0.0f, 0.0f,
+		PLAT_WIDTH * MultiplierW, PLAT_HEIGHT * MultiplierH, color, 0.0f, 0.0f,
+		-PLAT_WIDTH * MultiplierW, PLAT_HEIGHT * MultiplierH, color, 0.0f, 0.0f);
+
+	return AEGfxMeshEnd();
+}
+
+void RenderObject(Object pMesh)
+{
+	AEGfxSetRenderMode(pMesh.rm());
+	AEGfxSetPosition(pMesh.posX(), pMesh.posY());
+	AEGfxTextureSet(pMesh.tex(), pMesh.posU(), pMesh.posV());
+	AEGfxMeshDraw(pMesh.mesh(), AE_GFX_MDM_TRIANGLES);
+}
+
+void Object::SetMesh(AEGfxVertexList *pMesh)
+{
+	Mesh = pMesh;
+}
+void Object::SetPosition(float posX, float posY)
+{
+	X = posX;
+	Y = posY;
+}
+void Object::SetTex(AEGfxTexture *pTex, float posU, float posV)
+{
+	Tex = pTex;
+	U = posU;
+	V = posV;
+}
--- Production/DragonAttack/DragonAttack/Objects.h
+++ Production/DragonAttack/DragonAttack/Objects.h
@@ -1,7 +1,40 @@
 #pragma once
 #include "AEEngine.h"
 
+#define FLOOR_WIDTH 640.0f
+#define FLOOR_HEIGHT 100.0f
+#define PLAT_WIDTH 64.0f
+#define PLAT_HEIGHT 20.0f
+
+class Object {
+
+public:
+	void SetMesh(AEGfxVertexList *pMesh);
+	void SetPosition(float posX, float posY);
+	void SetTex(AEGfxTexture *pTex, float posU, float posV);
+	float posX() { return X; };
+	float posY() { return Y; };
+	float posU() { return U; };
+	float posV() { return V; };
+	AEGfxVertexList* mesh() { return Mesh; };
+	AEGfxRenderMode rm() { return RM; };
+	AEGfxTexture* tex() { return Tex; };
+private:
+	AEGfxVertexList *Mesh = nullptr;
+	AEGfxRenderMode RM = AE_GFX_RM_COLOR;
+	AEGfxTexture *Tex = nullptr;
+	float X = 0.0f;
+	float Y = 0.0f;
+	float U = 0.0f;
+	float V = 0.0f;
+};
+
 AEGfxVertexList* CreateSquare(float size, unsigned int color);
 
 AEGfxVertexList* CreateRectangle(float width, float height, unsigned int color);
 
+AEGfxVertexList* CreateFloor(float Multiplier, unsigned int color);
+
+AEGfxVertexList* CreatePlatform(float MultiplierW, float MultiplierH, unsigned int color);
+
+void RenderObject(Object pMesh);
