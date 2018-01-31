ed919e159aba4f239966bab54632cd3995bf6b1a
Javon Lee <javonleexw@gmail.com>
2018-01-31 17:30:41 +0800

    Collision Engine
    update Collision related files

---
+++ Production/DragonAttack/DragonAttack/Collision.cpp
@@ -0,0 +1,53 @@
+/* Start Header ************************************************************************/
+/*!
+\file       Collision.cpp
+\author     Javon Lee
+\par email: xiongweijavon.lee\@digipen.edu
+\brief
+Collision class functions are defined here.
+
+Copyright (C) 20xx DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents
+without the prior written consent of DigiPen Institute of
+Technology is prohibited.
+*/
+/* End Header **************************************************************************/
+
+#include "Collision.h" 
+
+bool Col_Comp::Rect_Rect(const Col_Comp &aabb_obj1, const Col_Comp &aabb_obj2)
+{
+	//check if the two objects are Rectangles 
+
+	if (aabb_obj1.shape != Rect || aabb_obj2.shape != Rect)
+	{
+		return false; // no collision if wrong function used. 
+	}
+
+
+	f32 left_A = aabb_obj1.min.x, left_B = aabb_obj2.min.x;
+	f32 right_A = aabb_obj1.max.x, right_B = aabb_obj2.max.x;
+	f32 top_A = aabb_obj1.max.y, top_B = aabb_obj2.max.y;
+	f32 btm_A = aabb_obj1.min.y, btm_B = aabb_obj2.min.y;
+
+
+	//Check for NO collision using AABB between 2 objects 
+	if (left_A > right_B) return false;
+	if (left_B > right_A) return false;
+	if (top_A < btm_B) return false;
+	if (top_B < btm_A) return false;
+	//else return false
+
+	return true;
+}
+
+bool Col_Comp::Circle_Circle(const Col_Comp &objA, const Col_Comp &objB)
+{
+	int RadiusSumSq = (objA.radius + objB.radius)*(objA.radius + objB.radius);
+	f32 DistCircleSq = (objA.mid.x - objB.mid.x)*(objA.mid.x - objB.mid.x) + (objA.mid.y - objB.mid.y)*(objA.mid.y - objB.mid.y);
+
+	if (DistCircleSq <= RadiusSumSq) return true; // There's collision
+
+	return false;
+
+}
---
+++ Production/DragonAttack/DragonAttack/Collision.h
@@ -0,0 +1,86 @@
+/* Start Header ************************************************************************/
+/*!
+\file       Collision.h
+\author     Javon Lee Xiong Wei
+\par email: xiongweijavon.lee\@digipen.edu
+\brief
+Collision component class declared here
+
+Copyright (C) 20xx DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents
+without the prior written consent of DigiPen Institute of
+Technology is prohibited.
+*/
+/* End Header **************************************************************************/
+#pragma once
+#include "AEEngine.h"
+
+enum Shape
+{
+	NoShape,
+	Point,
+	Rect,
+	Circle
+};
+
+enum CollisionFlag
+{
+	Collided,
+	Not_Collided
+};
+
+class Col_Comp
+{
+public:
+	Col_Comp(f32 mid_x, f32 mid_y, Shape shape) // constructor for point
+		: mid{ mid_x, mid_y }, shape{ shape }
+	{
+		if (shape != Point)
+			shape = NoShape;
+	}
+
+	Col_Comp(f32 min_x, f32 min_y, f32 max_x, f32 max_y, Shape shape) // constructor for rectangles/squares 
+		: min{ min_x , min_y }, max{ max_x, max_y }, shape{ shape }
+	{
+		if (shape != Rect)
+			shape = NoShape;
+	} //empty body constructor
+
+	Col_Comp(f32 mid_x, f32 mid_y, int radius, Shape shape) // constructor for circles 
+		: mid{ mid_x, mid_y }, radius{ radius }, shape{ shape }
+	{
+		if (shape != Circle)
+			shape = NoShape;
+	}
+
+	Col_Comp() {} // call default constructor if no parameters given. 
+
+	~Col_Comp() {}; //default dstor
+
+	bool Rect_Rect(const Col_Comp &aabb_obj1, const Col_Comp &aabb_obj2);
+
+	bool Circle_Circle(const Col_Comp &aabb_obj1, const Col_Comp &aabb_obj2);
+
+
+
+
+
+private:
+
+	AEVec2 min = { 0, 0 }; //represents btm left of a rect/sq
+
+	AEVec2 max = { 0, 0 }; //represents top right of a rect/sq
+
+	AEVec2 mid = { 0, 0 }; //represents the mid of a circle 
+
+	int radius = 0; //represents the radius of a circle 
+
+	Shape shape = NoShape;
+
+	CollisionFlag colflag = Not_Collided;
+
+};
+
+
+
+
