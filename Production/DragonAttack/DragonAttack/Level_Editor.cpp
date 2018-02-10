8cb8cf407973900e271028f282a9b9df3d45be61
Javon Lee <javonleexw@gmail.com>
2018-02-10 18:33:28 +0800

    INCOMPLETE code for LE

---
+++ Production/DragonAttack/DragonAttack/Level_Editor.cpp
@@ -0,0 +1,64 @@
+//#include <iostream>
+//
+//#include "Level_Editor.h"
+//
+//namespace
+//{
+//	static int ** Map_Array;
+//	static int Map_Width;
+//	static int Map_Height;
+//
+//
+//
+//	enum OBJ_TYPE
+//	{
+//		OBJ_TYPE_EMPTY, // 0
+//		OBJ_PLATFORM,   // 1
+//		OBJ_DRAGON,		// 2
+//		OBJ_GRUNT,		// 3
+//		OBJ_KING,		// 4
+//		OBJ_MERLIN,		// 5
+//		OBJ_LANCE,		// 6
+//	};
+//
+//}
+//
+//int Import_MapData(char* filename)
+//{
+//	FILE * Exported_file;
+//
+//	errno_t error = (fopen_s(&Exported_file, filename, "r"));
+//}
+//
+//namespace Level_Editor
+//{
+//	void Init(void)
+//	{
+//
+//	}
+//
+//	void Load(void)
+//	{
+//
+//	}
+//
+//	void Update(float dt)
+//	{
+//
+//	}
+//
+//	void Draw(void)
+//	{
+//
+//	}
+//
+//	void Free(void)
+//	{
+//
+//	}
+//
+//	void Unload(void)
+//	{
+//
+//	}
+//}
---
+++ Production/DragonAttack/DragonAttack/Level_Editor.h
@@ -0,0 +1,31 @@
+//#pragma once
+//#pragma once
+//#include "AEEngine.h"
+//#include "GameObject.h"
+//#include "Create_Object.h"
+//#include <cmath>
+//#include <vector>
+//
+//namespace Level_Editor
+//{
+//
+//	void Init(void);
+//
+//	void Load(void);
+//
+//	void Update(float dt);
+//
+
+//	void Draw(void);
+//
+//	void Free(void);
+//
+//	void Unload(void);
+//
+//}
+////determine necessary functions for Level Editor 
+////void SnapToCell(float *Coordinate);
+//
+////void FreeMapData(void); Got to free the allocated memory 
+//
+//int Import_MapData(char* filename); //Allocates the memory for the array 
