387b4e2fb3789fafa73c1b5c86864ee6fd29b224
everescent <35443878+everescent@users.noreply.github.com>
2018-02-25 21:11:27 +0800

    Added header

--- Production/DragonAttack/DragonAttack/Animation.cpp
+++ Production/DragonAttack/DragonAttack/Animation.cpp
@@ -1,3 +1,18 @@
+/* Start Header ************************************************************************/
+/*!
+\file       Animation.cpp
+\author     Jacob Lim
+\par email: jacob.lim\@digipen.edu
+\brief
+  Animation class member functions defined here.
+
+Copyright (C) 2018 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents
+without the prior written consent of DigiPen Institute of
+Technology is prohibited.
+*/
+/* End Header **************************************************************************/
+
 #include "Animation.h"
 #include <iostream>
 
--- Production/DragonAttack/DragonAttack/Animation.h
+++ Production/DragonAttack/DragonAttack/Animation.h
@@ -1,3 +1,18 @@
+/* Start Header ************************************************************************/
+/*!
+\file       Animation.h
+\author     Jacob Lim
+\par email: jacob.lim\@digipen.edu
+\brief
+  Animation class declared here.
+
+Copyright (C) 2018 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents
+without the prior written consent of DigiPen Institute of
+Technology is prohibited.
+*/
+/* End Header **************************************************************************/
+
 #pragma once
 #include <AEEngine.h>
 #include <functional>
