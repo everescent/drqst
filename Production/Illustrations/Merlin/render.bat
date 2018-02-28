3a3eb4f060534743537ee0d93f3813a21e81e88a
everescent <35443878+everescent@users.noreply.github.com>
2018-02-28 12:19:31 +0800

    Merlin texture

---
+++ Production/Illustrations/Merlin/render.bat
@@ -0,0 +1,7 @@
+@echo off
+set verbose=1
+set format=png
+set bits=64
+set out=G:\Documents\Digipen\Semester 02\GAM150\drqst\Production\Illustrations\Merlin\Energy Ball.png
+@echo Rendering G:\Documents\Digipen\Semester 02\GAM150\drqst\Production\Illustrations\Merlin\Energy Ball.png
+F:\Programs\Apophysis 2.0\hqi.exe < G:\DOCUME~1\Digipen\SEMEST~2\GAM150\drqst\PRODUC~1\ILLUST~1\Merlin\ENERGY~1.FLA
