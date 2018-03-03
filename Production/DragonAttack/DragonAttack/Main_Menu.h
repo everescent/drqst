fac29e0ed1fa4772572a870cc75d9b130a96e46f
Javon Lee <javonleexw@gmail.com>
2018-03-03 20:05:38 +0800

    Main_Menu done and Collision changed
    Did Main Menu and added static collision for point and rectangle

--- Production/DragonAttack/DragonAttack/Main_Menu.h
+++ Production/DragonAttack/DragonAttack/Main_Menu.h
@@ -1,10 +1,36 @@
+/* Start Header ************************************************************************/
+/*!
+\file       Main_Menu.h
+\author     Javon Lee
+\par email: xiongweijavon.lee\@digipen.edu
+\brief
+The Main menu (gamestate) function declarations are done here, as well as the 
+libraries and header files necessary to be included.
+
+Copyright (C) 20xx DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents
+without the prior written consent of DigiPen Institute of
+Technology is prohibited.
+*/
+/* End Header **************************************************************************/
 #pragma once
+#include "AEEngine.h"
+#include "Create_Object.h"
+#include "GameObject.h"
+#include "Camera.h"
+#include "GameStateManager.h"
+#include "Sprite.h"
+#include "GameObject.h"
+#include "Transform.h"
+#include "Collision.h"
+#include <utility>
+#include <iostream>
 
 namespace Main_Menu
 {
-	void Init(void);
-
 	void Load(void);
+
+	void Init(void);
 
 	void Update(float dt);
 
