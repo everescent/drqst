f4de67225a7b65f06709d46da3fda61539123947
Javon Lee <javonleexw@gmail.com>
2018-03-06 21:55:49 +0800

    Empty U.I file

---
+++ Production/DragonAttack/DragonAttack/UI.cpp
@@ -0,0 +1,14 @@
+/* Start Header ************************************************************************/
+/*!
+\file       UI.cpp
+\author     Javon Lee Xiong Wei
+\par email: xiongweijavon.lee\@digipen.edu
+\brief
+UI class functions defined here.
+
+Copyright (C) 2018 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents
+without the prior written consent of DigiPen Institute of
+Technology is prohibited.
+*/
+/* End Header **************************************************************************/
---
+++ Production/DragonAttack/DragonAttack/UI.h
@@ -0,0 +1,15 @@
+/* Start Header ************************************************************************/
+/*!
+\file       UI.h
+\author     Javon Lee Xiong Wei 
+\par email: xiongweijavon.lee\@digipen.edu
+\brief
+UI class body declared here.
+
+Copyright (C) 2018 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents
+without the prior written consent of DigiPen Institute of
+Technology is prohibited.
+*/
+/* End Header **************************************************************************/
+#pragma once
