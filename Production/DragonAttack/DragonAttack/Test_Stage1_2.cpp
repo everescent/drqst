f35e6010381063fc0a9b5dfd8c15c420a002ff34
SUFFERING\ACJH <acjh13@gmail.com>
2018-03-07 02:26:08 +0800

    1-1 & 1-2 level design

--- Production/DragonAttack/DragonAttack/Test_Stage1_2.cpp
+++ Production/DragonAttack/DragonAttack/Test_Stage1_2.cpp
@@ -1,40 +1,23 @@
 #include "Test_Stage1_2.h"
-#include "AEEngine.h"
-#include "Sprite.h"
-#include "Transform.h"
-#include "Create_Object.h"
-#include "GameObject.h"
-#include "Camera.h"
-#include "Collision.h"
-#include "GameStateManager.h"
-#include "Dragon.h"
-#include "Floor.h"
-#include "Wall.h"
-#include "Platform.h"
-#include "Barrier.h"
-#include "Tower.h"
-#include "Scarecrow.h"
-#include "AI_Data_Factory.h"
-#include "Level_Import.h"
-#include <utility>
-#include <iostream>
+
 namespace
 {
 	Dragon *player;
 	Sprite *BG;
 	Transform *M_BG;
+	Audio_Engine* Audio;
 
 	static int** MapData;
 	int Map_Width;
 	int Map_Height;
+
 	std::vector<Platform> platforms;
 	std::vector<Floor> floors;
-	//std::vector<Grunt> grunts;
 	std::vector<Wall> walls;
 	std::vector<Barrier> barriers;
-	std::vector<Scarecrow> scarecrows;
-	//std::vector<PickUp> powerups;
+	//std::vector<Scarecrow> scarecrows;
 
+	LevelChangePlatform *next;
 	Tower *archerTower;
 	PickUp *coin1, *coin2, *coin3, *hp;
 	Platform *up1, *up2;
@@ -49,14 +32,14 @@ namespace Test_Stage1_2
 		BG = new Sprite{ CreateBG(5.0f, "../../Illustrations/BG/BG_Stage1.png", 1.0f, 5.0f) };
 		M_BG = new Transform{};
 		player = new Dragon{};
+		Audio = new Audio_Engine{ 1, [](std::vector <std::string> &playlist)->void {playlist.push_back(".//Audio/Stage_1_BGM.mp3"); } };
 
 		if (!Import_MapData("level1-2.txt", MapData, Map_Width, Map_Height)) { AEGfxExit(); }
 
-		// Tower
-		archerTower = new Tower{ 7000.0f, 170.0f };
-
-		up1 = new Platform{ 6000.0f, -30.0f };
-		up2 = new Platform{ 6000.0f, -120.0f };
+		archerTower = new Tower        { 7000.0f,  170.0f };
+		up1 = new Platform             { 6000.0f,  -30.0f };
+		up2 = new Platform             { 6000.0f, -120.0f };
+		next = new LevelChangePlatform { 7500.0f,  150.0f };
 
 		coin1 = new PickUp{ S_CreateSquare(50.0f, "Textures/coin.png",1.0f),
 			Col_Comp{ 0.0f - 25.0f, 0.0f - 25.0f, 0.0f + 25.0f, 0.0f + 25.0f, Rect },
@@ -74,21 +57,24 @@ namespace Test_Stage1_2
 			Col_Comp{ 0.0f - 25.0f, 0.0f - 25.0f, 0.0f + 25.0f, 0.0f + 25.0f, Rect },
 			HP, 5450.0f , 200.0f };
 
-		c.push_back(Create_Basic_AI(GRUNT, AEVec2{ 727.0f , 105.0f }));
-		c.push_back(Create_Basic_AI(GRUNT, AEVec2{ 1895.0f , -165.0f }));
-		c.push_back(Create_Basic_AI(ARCHER, AEVec2{ 2335.0f , 195.0f }));
-		c.push_back(Create_Basic_AI(GRUNT, AEVec2{ 4500.0f , -345.0f }));
+		c.push_back(Create_Basic_AI(GRUNT , AEVec2{  727.0f ,  105.0f }));
+		c.push_back(Create_Basic_AI(GRUNT , AEVec2{ 1895.0f , -165.0f }));
+		c.push_back(Create_Basic_AI(ARCHER, AEVec2{ 2335.0f ,  195.0f }));
+		c.push_back(Create_Basic_AI(GRUNT , AEVec2{ 4500.0f , -345.0f }));
 		c.push_back(Create_Basic_AI(ARCHER, AEVec2{ 4700.0f , -345.0f }));
-		c.push_back(Create_Basic_AI(GRUNT, AEVec2{ 4900.0f , -345.0f }));
-		c.push_back(Create_Basic_AI(GRUNT, AEVec2{ 5550.0f , 195.0f }));
-		c.push_back(Create_Basic_AI(GRUNT, AEVec2{ 5300.0f , 195.0f }));
-		c.push_back(Create_Basic_AI(GRUNT, AEVec2{ 6450.0f , 195.0f }));
-		c.push_back(Create_Basic_AI(ARCHER, AEVec2{ 7000.0f, 400.0f }));
+		c.push_back(Create_Basic_AI(GRUNT , AEVec2{ 4900.0f , -345.0f }));
+		c.push_back(Create_Basic_AI(GRUNT , AEVec2{ 5550.0f ,  195.0f }));
+		c.push_back(Create_Basic_AI(GRUNT , AEVec2{ 5300.0f ,  195.0f }));
+		c.push_back(Create_Basic_AI(GRUNT , AEVec2{ 6450.0f ,  195.0f }));
+		c.push_back(Create_Basic_AI(ARCHER, AEVec2{ 6950.0f ,  300.0f }));
 	}
 
 
 	void Init(void)
 	{
+		Audio->Play(0);
+		Audio->SetLoop(0, 1);
+
 		for(size_t i = 0; i < c.size(); ++i)
 			c[i]->SetActive(true);
 
@@ -115,13 +101,13 @@ namespace Test_Stage1_2
 					float f_x = (float)x;
 					float f_y = (float)y;
 					grunts.push_back(Grunt{ Convert_X(f_x) , Convert_Y(f_y) });
-				}*/
+				}
 				if (MapData[y][x] == OBJ_SC)
 				{
 					float f_x = (float)x;
 					float f_y = (float)y;
 					scarecrows.push_back(Scarecrow{ Convert_X(f_x) , Convert_Y(f_y) });
-				}
+				}*/
 				if (MapData[y][x] == OBJ_WALL)
 				{
 					float f_x = (float)x;
@@ -160,13 +146,14 @@ namespace Test_Stage1_2
 				}*/
 			}
 		}
-
 	}
 
 
 
 	void Update(float dt)
 	{
+		Audio->Update();
+
 		for (size_t i = 0; i < c.size(); ++i)
 			c[i]->Update(*player, dt);
 
@@ -181,10 +168,10 @@ namespace Test_Stage1_2
 				elem.Update(*(c[i]), dt);
 			}
 
-			for (Scarecrow& elem3 : scarecrows)
+			/*for (Scarecrow& elem3 : scarecrows)
 			{
 				elem.Update(elem3, dt);
-			}
+			}*/
 			elem.Update(*player, dt);
 		}
 		for (Wall& elem : walls)
@@ -193,19 +180,13 @@ namespace Test_Stage1_2
 			{
 				elem.Update(*(c[i]), dt);
 			}
-
-
 			elem.Update(*player, dt);
 		}
 		for (Barrier& elem : barriers)
 		{
 			elem.Update(*player, dt);
-		}
+		}/*
 		for (Scarecrow& elem : scarecrows)
-		{
-			elem.Update(*player, dt);
-		}
-		/*for (PickUp& elem : powerups)
 		{
 			elem.Update(*player, dt);
 		}*/
@@ -216,6 +197,7 @@ namespace Test_Stage1_2
 		archerTower->Update(*player, dt);
 		up1->Update(*player, dt);
 		up2->Update(*player, dt);
+		next->Update(*player, dt);
 		player->Update(*player, dt);
 
 		std::cout << (int)player->PosX <<", "<< (int)player->PosY << std::endl;
@@ -228,7 +210,6 @@ namespace Test_Stage1_2
 		BG->Render_Object(*M_BG);
 
 		archerTower->Render();
-		archerTower->Sprite_.SetAlphaTransBM(1.0f, 1.0f, AE_GFX_BM_BLEND);
 		up1->Render();
 		up2->Render();
 
@@ -239,41 +220,28 @@ namespace Test_Stage1_2
 		for (Floor& elem : floors)
 		{
 			elem.Render();
-		}
-		/*for (Wall& elem : walls)
-		{
-		elem.Render();
-		}*/
+		}/*
 		for (Scarecrow& elem : scarecrows)
 		{
 			elem.Render();
-			elem.Sprite_.SetAlphaTransBM(1.0f, 1.0f, AE_GFX_BM_BLEND);
-		}
+		}*/
 		for (Barrier& elem : barriers)
 		{
 			elem.Render();
-			elem.Sprite_.SetAlphaTransBM(1.0f, 1.0f, AE_GFX_BM_BLEND);
 		}
-	/*	for (PickUp& elem : powerups)
+		for (size_t i = 0; i < c.size(); ++i)
 		{
-			elem.Render();
-			elem.Sprite_.SetAlphaTransBM(1.0f, 1.0f, AE_GFX_BM_BLEND);
-		}*/
+			c[i]->Render();
+		}
+		coin1->Render();
+		coin2->Render();
+		coin3->Render();
+
+		next->Render();
 
 		player->Render();
 		player->Sprite_.SetAlphaTransBM(1.0f, 1.0f, AE_GFX_BM_BLEND);
 
-		for (size_t i = 0; i < c.size(); ++i)
-		{
-			c[i]->Render();
-			c[i]->Sprite_.SetAlphaTransBM(1.0f, 1.0f, AE_GFX_BM_BLEND);
-		}
-		coin1->Render();
-		coin1->Sprite_.SetAlphaTransBM(1.0f, 1.0f, AE_GFX_BM_BLEND);
-		coin2->Render();
-		coin2->Sprite_.SetAlphaTransBM(1.0f, 1.0f, AE_GFX_BM_BLEND);
-		coin3->Render();
-		coin3->Sprite_.SetAlphaTransBM(1.0f, 1.0f, AE_GFX_BM_BLEND);
 	}
 
 	void Free(void)
@@ -281,13 +249,15 @@ namespace Test_Stage1_2
 		delete BG;
 		delete M_BG;
 		delete player;
-		// Static Objs
+		delete Audio;
+
 		delete archerTower;
 		delete up1;
 		delete up2;
 		delete coin1;
 		delete coin2;
 		delete coin3;
+		delete next;
 		c.clear();
 	}
 
