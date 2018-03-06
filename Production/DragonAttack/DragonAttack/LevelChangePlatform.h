acab89bc86bd8b83967b7764ab57470b2a515533
SUFFERING\ACJH <acjh13@gmail.com>
2018-03-07 02:25:45 +0800

    Added level change platforms + filters

---
+++ Production/DragonAttack/DragonAttack/LevelChangePlatform.h
@@ -0,0 +1,31 @@
+/* Start Header ************************************************************************/
+/*!
+\file       LevelChangePlatform.h
+\author     Andrew Chong
+\par email: c.jiahaoandrew\@digipen.edu
+\brief
+Character class, parent class of the characters to be created
+
+Copyright (C) 2018 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents
+without the prior written consent of DigiPen Institute of
+Technology is prohibited.
+*/
+/* End Header **************************************************************************/
+
+#pragma once
+#include "GameObject.h"
+#include "Transform.h"
+#include "Dragon.h"
+#include "Create_Object.h"
+#include "GameStateManager.h"
+
+// A class that creates a basic character
+class LevelChangePlatform : public GameObject
+{
+private:
+
+public:
+	LevelChangePlatform(float x, float y); // default constructor
+	void Update(Dragon &player, const float &dt);
+};
