9107f4c2596192a7f7a8cc03eed11d36f1748f4c
simdis1 <35450445+simdis1@users.noreply.github.com>
2018-03-26 22:51:09 +0800

    going to start on credits

---
+++ Production/DragonAttack/DragonAttack/Credits.cpp
@@ -0,0 +1,38 @@
+#include "Credits.h"
+
+// for global variables
+namespace
+{
+
+}
+
+namespace Credits
+{
+	void Init(void)
+	{
+	}
+
+	void Load(void)
+	{
+	}
+
+	void Update(float dt)
+	{
+
+	}
+
+	void Draw(void)
+	{
+
+	}
+
+	void Free(void)
+	{
+
+	}
+
+	void Unload(void)
+	{
+
+	}
+}
--- Production/DragonAttack/DragonAttack/Credits.h
+++ Production/DragonAttack/DragonAttack/Credits.h
@@ -1,1 +1,18 @@
 #pragma once
+#include "AEEngine.h"
+#include "Camera.h"
+
+namespace Credits
+{
+	void Init(void);
+
+	void Load(void);
+
+	void Update(float dt);
+
+	void Draw(void);
+
+	void Free(void);
+
+	void Unload(void);
+}
