e77fb89052a9227cb04082e7b42f48e034c9f9d2
everescent <35443878+everescent@users.noreply.github.com>
2018-04-10 16:41:48 +0800

    Changed to shift

--- Production/DragonAttack/DragonAttack/Dragon.cpp
+++ Production/DragonAttack/DragonAttack/Dragon.cpp
@@ -116,7 +116,7 @@ void Dragon::Input()
     Attack = true;
   }
 
-  if (AEInputCheckTriggered(AEVK_P))
+  if (AEInputCheckTriggered(AEVK_RSHIFT || AEInputCheckTriggered(AEVK_LSHIFT)))
   {
     if (Charge == Max_Charge)
      {
