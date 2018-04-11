632bfeb001950b181669f9b7e2dea951c8815a99
simdis1 <simdis1.williamykh@hotmail.com>
2018-04-11 12:38:42 +0800

    adding comments to files

--- Production/DragonAttack/DragonAttack/King_Arthur.h
+++ Production/DragonAttack/DragonAttack/King_Arthur.h
@@ -14,15 +14,16 @@ Technology is prohibited.
 /* End Header **************************************************************************/
 #pragma once
 
-#include "Characters.h"
-#include "Boss_Phase.h"
-#include "Boss_States.h"
-#include "Dragon.h"
+#include "Characters.h"  // parent class
+#include "Boss_Phase.h"  // boss_phase
+#include "Boss_States.h" // boss_attack, Boss_Action_State
+#include "Dragon.h"      // player class
 
 class King_Arthur : public Characters
 {
 private:
-    enum KA_MoveSet // for switch case to determine which attack gets called
+    // enumerators for king arthur attacks
+	enum KA_MoveSet
     {
         SINGLE_SLASH = 0,
         TRIPLE_SLASH,
@@ -32,56 +33,86 @@ private:
         NONE
     };
 
-    void King_Arthur_Phase2(const float dt); // changes mechanics for phase 2
-    void King_Arthur_Phase3(const float dt);
-
+    //------------------------------------------------------------------
+    //
+    // INITIALIZERS FOR KING ARTHUR
+    //
+    //------------------------------------------------------------------
     void Init_KA_Attacks(void);    // initialize king arthur attacks
     void Init_MobArray(void);      // initialize the mob array for phase 2
     void Init_Particle(void);      // initializes the various particle effects ka will have
-    void Update_Particle(const float dt);
-
-    void Set_Forward_Dir(const Dragon&);
-    void Off_Particles(void);
     
-    // boss behaviours /////////////////////////////////////////////////
+    //------------------------------------------------------------------
+    //
+    // HELPER FUNCTIONS FOR KING ARTHUR
+    //
+    //------------------------------------------------------------------
+    void Set_Forward_Dir(const Dragon&);     // set the direction king arthur should face 
+    void Off_Particles(void);                // set all particle active to false
+	void Update_Particle(const float dt);    // update the particle system
+	void King_Arthur_Phase2(const float dt); // changes mechanics for phase 2
+    void King_Arthur_Phase3(const float dt); // changes mechanics for phase 3
+    
+    //------------------------------------------------------------------
+    //
+    // BEHAVIOR FOR KING ARTHUR
+    //
+    //------------------------------------------------------------------
     void Idle(const Dragon &d, const float dt);
     void Moving(const Dragon &d, const float dt);
     void Attack(Dragon &d, const float dt);
-    // boss behaviours end /////////////////////////////////////////////
 
-    // attack functions /////////////////////////////////////////////////
+    //------------------------------------------------------------------
+    //
+    // ATTACKS FOR KING ARTHUR
+    //
+    //------------------------------------------------------------------
     void Dash_Attack(Dragon &d, const float dt);     // jump attack
     void Single_Slash(Dragon &d, const float dt);    // single slash + wave
     void Triple_Slash(Dragon &d, const float dt);    // single slash 3 times
     void Heal_and_Spawn(Dragon &d, const float dt);  // phase 2, heal and spawm mobs
     void Spinning_Blades(Dragon &d, const float dt); // rotating swords that attacks player
-    // attack functions /////////////////////////////////////////////////
 
-    BOSS_PHASE                ka_phase;
-    Particle_System *         healing_effect;
-    Particle_System *         sword_effect;
-    Particle_System *         slash_effect[3];
-    Particle_System *         phase_effect;
-    KA_MoveSet                currAttk;
-    Animation		          anim;
-    Boss_Action_State         current_action; // different states of boss arthur
-    std::vector <Characters*> mobs;           //an array to store the mobs to be spawn
-    float                     timer;          // for phase transition
-    float                     mob_timer;      // render the mobs base on time
-    Audio_Engine              music;          // music for king arthur
-    char                      active_mobs;    // number of mobs to spawn
-    bool                      spawn_mob;      // check when to update and render mob
-    char                      behavior_swap;  // switch between idling and moving
-    bool                      healing;        // determine if king arthur is healing
+	//------------------------------------------------------------------
+    //
+    // VARIABLES FOR KING ARTHUR
+    //
+    //------------------------------------------------------------------
+    BOSS_PHASE                ka_phase;           // current phase of king arthur
+    Particle_System *         healing_effect;     // particle system for healing effect
+    Particle_System *         sword_effect;       // particle system for the swords in phase 3
+    Particle_System *         slash_effect[3];    // particle system for the slashes
+    Particle_System *         phase_effect;       // particle system for phase transition
+    KA_MoveSet                currAttk;           // current attack king arthur is using
+    Animation		          anim;               // king arthur's animation
+    Boss_Action_State         current_action;     // different states of boss arthur
+    std::vector <Characters*> mobs;               //an array to store the mobs to be spawn
+    float                     timer;              // for phase transition
+    float                     mob_timer;          // render the mobs base on time
+    Audio_Engine              music;              // music for king arthur
+    char                      active_mobs;        // number of mobs to spawn
+    bool                      spawn_mob;          // check when to update and render mob
+    char                      behavior_swap;      // switch between idling and moving
+    bool                      healing;            // determine if king arthur is healing
 
 public:
-    King_Arthur(Sprite*);
-
-    ~King_Arthur(); // destructor
-    void Update(Dragon &player, const float dt);
-    void Render();
-    void Dead(void);
-    BOSS_PHASE Get_Phase(void) const;
-    std::vector <Characters*>& Get_Mobs(void);
+    //------------------------------------------------------------------
+    //
+    //  CONSTRUCTOR AND DESTRUCTOR
+    //
+    //------------------------------------------------------------------
+	King_Arthur(Sprite*);
+    ~King_Arthur(); 
+	
+	//------------------------------------------------------------------
+    //
+    //  PUBLIC FUNCTIONS
+    //
+    //------------------------------------------------------------------
+    void Update(Dragon &player, const float dt);  // updates king arthur
+    void Render();                                // render king arthur
+    void Dead(void);                              // update king arthur when he dies
+    BOSS_PHASE Get_Phase(void) const;             // get the current phsae of king arthur
+    std::vector <Characters*>& Get_Mobs(void);    // get the vector storing the mobs
 };
 
--- Production/DragonAttack/DragonAttack/Lancelot.h
+++ Production/DragonAttack/DragonAttack/Lancelot.h
@@ -14,37 +14,61 @@ Technology is prohibited.
 /* End Header **************************************************************************/
 #pragma once
 
-#include "Characters.h"
-#include "Dragon.h"
-#include "Boss_Phase.h"
-#include "Boss_States.h"
+#include "Characters.h"   // parent class of lancelot
+#include "Dragon.h"       // player class
+#include "Boss_Phase.h"   // enum for lancelot phase
+#include "Boss_States.h"  // boss_attack, boss_action_state
 
 class Lancelot : public Characters
 {
 private:
-  void Idle(const Dragon &  , const float dt); // lancelot idle state
-  void Moving(const Dragon& , const float dt); // moving state of lancelot
-  void Attack(Dragon &      , const float dt); // attack state of lancelot
+  //------------------------------------------------------------------
+  //
+  //  BEHAVIOUR FOR LANCELOT
+  //
+  //------------------------------------------------------------------
+  void Idle(const Dragon &  , const float dt); // Lancelot idle state
+  void Moving(const Dragon& , const float dt); // moving state of Lancelot
+  void Attack(Dragon &      , const float dt); // attack state of Lancelot
   void Dead(void);
 
-  void Init(void); //initializer for lancelot
-  void Init_Stab(void);
-  void Init_Slash(void);
-  void Init_Arondight(void);
-  void Init_Particles(void);
-  void Update_Particles(const float dt);
-  void Set_Face_Dir(const Dragon &);
-  void Set_Attk_Dir(void); // set the attack direction of lancelot
+  //------------------------------------------------------------------
+  //
+  //  INITIALIZERS FOR LANCELOT VARIABLES
+  //
+  //------------------------------------------------------------------
+  void Init(void);             // initializer for Lancelot
+  void Init_Stab(void);        // initialize the variables for stab attack
+  void Init_Slash(void);       // initialize the variables for slash attack
+  void Init_Arondight(void);   // initialize the variables for Arondight
+  void Init_Particles(void);   // initialize the particle systems used by Lancelot
+  
+  //------------------------------------------------------------------
+  //
+  //  HELPER FUNCTIONS FOR LANCELOT
+  //
+  //------------------------------------------------------------------
+  void Set_Face_Dir(const Dragon &);     // set the texture facing direction
+  void Set_Attk_Dir(void);               // set the attack direction of Lancelot
+  void Lancelot_Phase2(const float dt);  // set Lancelot to go to phase 2
+  void Update_Particles(const float dt); // updates the particle systems
 
-  void Lancelot_Phase2(const float dt);
-
-  void Stab(Dragon&, const float dt); // first basic attack of lancelot
-  void Slash(Dragon&, const float dt); // second basic attack of lancelot
-  void Mad_Enhancement(const float dt); // makes lancelot move faster, shorter cooldown
+  //------------------------------------------------------------------
+  //
+  //  LANCELOT ATTACK FUNCTIONS
+  //
+  //------------------------------------------------------------------
+  void Stab(Dragon&, const float dt);      // first basic attack of  Lancelot
+  void Slash(Dragon&, const float dt);     // second basic attack of Lancelot
+  void Mad_Enhancement(const float dt);    // makes Lancelot move faster, shorter cooldown
   void Arondight(Dragon&, const float dt); // phase 2 mechanism, long range attack
 
 
-  // variables use for lancelot
+  //------------------------------------------------------------------
+  //
+  //  LANCELOT VARIABLES
+  //
+  //------------------------------------------------------------------
   BOSS_PHASE        phase;                   // flag for lancelot phase
   bool              M_E;                     // lancelot berserk state
   Particle_System  *arondight_particle;      // particle system for arondight
@@ -56,8 +80,19 @@ private:
   Audio_Engine      music;                   // music for lancelot
 
 public:
+  //------------------------------------------------------------------
+  //
+  //  CONSTRUCTOR AND DESTRUCTOR
+  //
+  //------------------------------------------------------------------
   Lancelot(Sprite*);  // default constructor for lancelot
-  ~Lancelot(); // destructor for lancelot
+  ~Lancelot();        // destructor for lancelot
+  
+  //------------------------------------------------------------------
+  //
+  //  UPDATE AND RENDER FUNCTIONS FOR LANCELOT
+  //
+  //------------------------------------------------------------------
   void Update(Dragon &player, const float dt);
   void Render(void);
 
